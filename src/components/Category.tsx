c1a429d95d2bada2cbd61e3aa6442359b8df1790
AcuacheY <v.acuache15@gmail.com>
2025-05-23 00:45:04 -0500

    Corregido desbordamiento 2h

--- src/components/Category.tsx
+++ src/components/Category.tsx
@@ -4,7 +4,7 @@ import ropaHomnre from '../assets/images/ropa-de-hombre.png'
 import tecnologia from '../assets/images/tecnologia.png'
 export default function Category() {
     return (
-        <section className='max-w-7xl bg-BGGray-light p-7 rounded-2xl m-auto'>
+        <section className='max-w-7xl bg-BGGray-light p-7 rounded-2xl m-auto overflow-hidden'>
             <h2 className='text-3xl font-bold text-center mb-5'>BUSCAR POR CATEGORÍA</h2>
             <div className='transition-all duration-300 w-full max-w-7xl grid grid-cols-1 auto-rows-[180px] gap-y-5 md:grid-cols-2 sm:gap-x-5 md:p-10 lg:grid-cols-3'>
                 <article className='transition-all duration-300 bg-BGWhite relative rounded-xl overflow-hidden'>
